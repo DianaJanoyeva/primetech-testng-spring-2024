9f124c9cc8218046477102bbc0f4062088e84ac7
DianaJanoyeva <dinajahna@gmail.com>
2024-10-15 19:00:41 -0500

    adding some code

--- src/test/java/testcases/Calculator.java
+++ src/test/java/testcases/Calculator.java
@@ -1,4 +1,29 @@
 package testcases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Calculator {
+
+    @Test
+    public void calculatorTest(){
+        System.out.println("hey");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Hello");
+    }
+
+    @Test
+    public void loginWithValidCredentials(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("sauce demo url");
+        WebElement username = driver.findElement(By.xpath("xpath"));
+    }
+
+
 }
